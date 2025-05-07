59ec6bbcf4ac0f4fc84de49e25fec03d5a4b7bb6
Neyamat Ullah <neyamat7.ullah@gmail.com>
2025-05-07 20:26:43 +0600

    remove extra code that is not used

--- src/Layouts.jsx
+++ src/Layouts.jsx
@@ -1,26 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router";
+import React from "react";
+import { Outlet } from "react-router";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { ToastContainer } from "react-toastify";
-import Loading from "./components/Loading/Loading";
 
 const Layouts = () => {
-  const [isPageLoading, setIsPageLoading] = useState(true);
-  const { key } = useLocation();
-
-  useEffect(() => {
-    setIsPageLoading(true);
-    const loadingTime = setTimeout(() => setIsPageLoading(false), 300);
-
-    return () => clearTimeout(loadingTime);
-  }, [key]);
-
   return (
     <div>
       <Navbar></Navbar>
-      {isPageLoading ? <Loading></Loading> : <Outlet></Outlet>}
-
+      <Outlet></Outlet>
       <Footer></Footer>
       <ToastContainer autoClose={3000}></ToastContainer>
     </div>
